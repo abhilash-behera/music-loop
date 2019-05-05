404c06f7140455250f32188ad4f814ed74ef2525
abhilima <abhilash@tekolutions.ai>
2019-05-05 13:17:19 +0530

    added dot env

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+const mongodbUri =
+  process.env.MONGODB_URI || "mongodb://localhost/music-loop-dev";
+
+
