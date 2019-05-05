8d43f7ad9bccd27d1f3c098b6d9abc326f130a88
abhilima <abhilash@tekolutions.ai>
2019-05-05 13:22:22 +0530

    defined auth middleware and server start script

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,23 @@ const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 const mongodbUri =
   process.env.MONGODB_URI || "mongodb://localhost/music-loop-dev";
+const auth=require('./routes/auth');
+const port=Number(process.env.PORT)||3000;
 
+app.use('/auth',auth);
 
+mongoose.connect(mongodbUri,{useNewUrlParser:true},err=>{
+    if(err){
+        console.log('Error in connecting to database: ',err);
+    }else{
+        console.log('Successfully connected to database: ',mongodbUri);
+    }
+})
+
+app.listen(port,err=>{
+    if(err){
+        console.log('Error in starting server: ',err);
+    }else{
+        console.log('Server started successfully on port: ',port);
+    }
+})
